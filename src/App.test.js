dd897ffaf3b9e0a03d99c5ad124637152b77abfd
JZachrisson <jesper.zachrisson@gmail.com>
2020-12-27 14:26:04 +0100

    setting up jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import App from './App';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() });
+
+test('renders learn react link', () => {});
