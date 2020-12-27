060b7c45544be6e98663e0c677b02aacce4b9bc0
JZachrisson <jesper.zachrisson@gmail.com>
2020-12-27 14:49:47 +0100

    setting up render tests

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
-import App from './App';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-test('renders learn react link', () => {});
---
+++ src/Congrats.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Congrats = () => {
+  return <div></div>;
+};
+
+export default Congrats;
