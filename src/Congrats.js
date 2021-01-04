e6727361caf64d7a5bb757af4aa77072995f1d38
JZachrisson <jesper.zachrisson@gmail.com>
2021-01-04 12:30:05 +0100

    passing tests for proptypes

--- src/Congrats.js
+++ src/Congrats.js
@@ -15,4 +15,8 @@ const Congrats = (props) => {
   }
 };
 
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
+};
+
 export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -6,8 +6,11 @@ import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const defaultProps = { success: false };
+
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test('renders without error', () => {
