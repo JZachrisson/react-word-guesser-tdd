aee5a645c364ffcf790df491a3628a974b882a22
JZachrisson <jesper.zachrisson@gmail.com>
2021-01-05 13:34:05 +0100

    adding guessedword component and some tests

---
+++ src/GuessedWord.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+};
+
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+});
---
+++ src/GuessedWords.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => {
+  return <div />;
+};
+
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default GuessedWords;
