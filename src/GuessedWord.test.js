4ffda21339fd4cac86bed1a2f73fa65bd8ad1d0c
JZachrisson <jesper.zachrisson@gmail.com>
2021-01-22 09:32:15 +0100

    start tests for guessedwords

--- src/GuessedWord.test.js
+++ src/GuessedWord.test.js
@@ -14,3 +14,20 @@ const setup = (props = {}) => {
 test('does not throw warning with expected props', () => {
   checkProps(GuessedWords, defaultProps);
 });
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+describe('if there are words guessed', () => {});
--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuessedWords = (props) => {
-  return <div />;
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 GuessedWords.propTypes = {
