bb54436ee5db027931f931a1f14768b9b18f4a60
JZachrisson <jesper.zachrisson@gmail.com>
2021-01-22 10:00:35 +0100

    adding some styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+      />
+    </div>
+  );
 }
 
 export default App;
